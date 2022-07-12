72b221a75bd1af304dd842ab3069a82b112f50cb
ibouzidi <bouzidi.idris42@gmail.com>
2022-07-12 10:59:45 +0200

    add generic list and detail view.

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Tasklist
+from .views import TaskList, TaskDetail
 
 urlpatterns = [
-    path('', Tasklist.as_view(), name='tasks'),
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task_detail'),
 ]
