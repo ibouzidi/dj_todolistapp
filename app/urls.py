18e60ad0099d3d7d5575becaa5029c1dc0eb7bb4
ibouzidi <bouzidi.idris42@gmail.com>
2022-07-12 14:38:59 +0200

    add generic authentication and search form.

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, \
+    CustomLoginView, RegisterPage
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
@@ -7,4 +9,9 @@ urlpatterns = [
     path('task/create/', TaskCreate.as_view(), name='task_create'),
     path('task/update/<int:pk>/', TaskUpdate.as_view(), name='task_update'),
     path('task/delete/<int:pk>/', TaskDelete.as_view(), name='task_delete'),
+
+    # Login urls
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
 ]
